8740e08c02beaee661cec4e3cbbb1e405ac875bd
Nikolai Trusov <nickolaytrusov01@gmail.com>
2025-09-10 16:54:56 +0300

    feat: add from_orm_retort

---
+++ src/app/infrastructure/db/mappers/from_orm.py
@@ -0,0 +1,14 @@
+from adaptix._internal.conversion.facade.provider import coercer
+from adaptix._internal.conversion.facade.retort import ConversionRetort
+
+from app.domain.value_objects.user_id import UserId
+from app.domain.value_objects.user_password_hash import UserPasswordHash
+from app.domain.value_objects.username import Username
+
+from_orm_retort = ConversionRetort(
+    recipe=[
+        coercer(int, UserId, lambda x: UserId(x)),
+        coercer(str, Username, lambda x: Username(x)),
+        coercer(bytes, UserPasswordHash, lambda x: UserPasswordHash(x)),
+    ]
+)
--- src/app/infrastructure/db/user_gateway.py
+++ src/app/infrastructure/db/user_gateway.py
@@ -5,7 +5,9 @@ from app.application.common.ports.user_gateway import UserGateway
 from app.domain.entities.user import User
 from app.domain.value_objects.user_id import UserId
 from app.domain.value_objects.username import Username
-from app.infrastructure.db.mappers.orm_to_dto import dto_to_orm_retort
+from app.infrastructure.db.mappers.dto_to_orm import dto_to_orm_retort
+from app.infrastructure.db.mappers.from_orm import from_orm_retort
+from app.infrastructure.db.models import UserORM
 from app.infrastructure.db.types import MainAsyncSession
 
 
@@ -14,9 +16,12 @@ class UserSQLGateway(UserGateway):
     session: MainAsyncSession
 
     async def add(self, user: CreateUser) -> User:
-        new_user = dto_to_orm_retort(user)
+        converter = dto_to_orm_retort.get_converter(CreateUser, UserORM)
+        new_user = converter(user)
         self.session.add(new_user)
         await self.session.flush()
+        user_converter = from_orm_retort.get_converter(UserORM, User)
+        return user_converter(new_user)
 
     async def read_by_id(self, user_id: UserId) -> User | None:
         raise NotImplementedError
